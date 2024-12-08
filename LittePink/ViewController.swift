90d35c66be92098ee8cbeb582ddd848196fa9bbf
Jianing-Chuo <76877706+Jianing-Chuo@users.noreply.github.com>
2024-12-08 17:05:17 +0700

    Initial Commit

---
+++ LittePink/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LittePink
+//
+//  Created by 啜加宁 on 2024/12/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
